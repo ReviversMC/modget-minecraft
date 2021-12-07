884b9983aa34b41606c5df3ee9ede5bd708739bd
NebelNidas <burnerjulian@gmail.com>
2021-12-07 17:00:59 +0100

    Use method name from the older mappings

--- src/main/java/com/github/reviversmc/modget/minecraft/mixin/ModsCheckMixin.java
+++ src/main/java/com/github/reviversmc/modget/minecraft/mixin/ModsCheckMixin.java
@@ -3,16 +3,17 @@ package com.github.reviversmc.modget.minecraft.mixin;
 import com.github.reviversmc.modget.minecraft.util.Utils;
 import com.terraformersmc.modmenu.gui.ModsScreen;
 import com.terraformersmc.modmenu.gui.widget.ModMenuTexturedButtonWidget;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 @Mixin(ModsScreen.class)
 public abstract class ModsCheckMixin extends Screen {
@@ -29,7 +30,7 @@ public abstract class ModsCheckMixin extends Screen {
     @Inject(method = "init", at = @At("TAIL"))
     public void onInit(CallbackInfo ci) {
         int searchBoxWidth = this.paneWidth - 32 - 22;
-        this.addDrawableChild(new ModMenuTexturedButtonWidget(this.paneWidth / 2 + searchBoxWidth / 2 + 14,
+        this.addChild(new ModMenuTexturedButtonWidget(this.paneWidth / 2 + searchBoxWidth / 2 + 14,
                 22, 20, 20, 0, 0, UPDATE_BUTTON_LOCATION, 32, 64,
                 button -> Utils.showToast(new LiteralText("Feature in development!"), new LiteralText("We are working on it")), LiteralText.EMPTY,
                 (button, matrices, mouseX, mouseY) -> {
